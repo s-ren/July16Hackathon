a426ec9ad1e4c702c963aadab62dae0260891159
s-ren <silei_ren@brown.edu>
2016-07-16 14:00:02 -0700

    parser for security

--- Project/src/Parser.java
+++ Project/src/Parser.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Map;
 
@@ -5,7 +8,29 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class Parser {
-	public Map<String, Object> parse(String jsonStr) {
+	private static BufferedReader reader;
+
+	public static void readSecurity(String name) throws IOException {
+		String line = read(name);
+    Map<String, Object> sec = parse(line);
+    if (name.equals("BOND")) Security.BOND = new Security("BOND", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else if (name.equals("VALBZ")) Security.VALBZ = new Security("VALBZ", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else if (name.equals("VALE")) Security.VALE = new Security("VALE", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else if (name.equals("GS")) Security.GS = new Security("GS", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else if (name.equals("MS")) Security.MS = new Security("MS", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else if (name.equals("WFC")) Security.WFC = new Security("WFC", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else if (name.equals("XLF")) Security.XLF = new Security("XLF", (double[][]) sec.get("sell"), (double[][]) sec.get("buy"));
+    else throw new IOException("invalid security name!");
+	}
+	
+	private static String read(String fileName) throws IOException {
+		reader = new BufferedReader(new FileReader(fileName));
+		String line = reader.readLine();
+    reader.close();
+    return line;
+	}
+	
+	private static Map<String, Object> parse(String jsonStr) {
 		Type typeOfObjectsList = new TypeToken<Map<String, Object>>() {}.getType();
 		Map<String, Object> out = new Gson().fromJson(jsonStr, typeOfObjectsList);
 		return out;
--- Project/src/Trader.java
+++ Project/src/Trader.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -7,8 +8,25 @@ public class Trader
 {
 	public PrintWriter to_exchange;
 	public BufferedReader from_exchange;
-	public void simpleBuyBond(){
-   		to_exchange.println("HELLO RDFZFIFTEEN"); 
-		to_exchange.println("ADD 1 BOND BUY 999 10");
+	public void simpleBuyBond() throws IOException{
+		for (int i = 0; i < 10; i++) {
+			Parser.readSecurity("BOND");
+			Parser.readSecurity("VALBZ");
+			Parser.readSecurity("VALE");
+			Parser.readSecurity("GS");
+			Parser.readSecurity("MS");
+			Parser.readSecurity("WFC");
+			Parser.readSecurity("XLF");
+		}
+		System.out.println(Security.BOND);
+		System.out.println(Security.VALBZ);
+		System.out.println(Security.VALE);
+		System.out.println(Security.GS);
+		System.out.println(Security.MS);
+		System.out.println(Security.WFC);
+		System.out.println(Security.XLF);
+		
+   	//to_exchange.println("HELLO RDFZFIFTEEN");
+		//to_exchange.println("ADD 1 BOND BUY 999 10");
 	}
 }
