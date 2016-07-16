1f296a5fccaf9e378bed5c14d2e453c6bce29c9d
s-ren <silei_ren@brown.edu>
2016-07-16 12:19:57 -0700

    add parser

--- Project/src/Bot.java
+++ Project/src/Bot.java
@@ -1,5 +1,3 @@
-//package janeStreet;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -11,7 +9,7 @@ public class Bot
 	private Socket skt;
 	private BufferedReader from_exchange;
 	private PrintWriter to_exchange;
-	public init(){
+	public void init(){
         skt = new Socket("test-exch-rdfzfifteen", 20000);
         from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
         to_exchange = new PrintWriter(skt.getOutputStream(), true);
@@ -23,9 +21,9 @@ public class Bot
             Socket skt = new Socket("test-exch-rdfzfifteen", 20000);
             BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
             PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
-			Trader trader = new Trader();
-			trader.to_exchange = to_exchange;	
-	   		to_exchange.println("HELLO RDFZFIFTEAN"); 
+            Trader trader = new Trader();
+            trader.to_exchange = to_exchange;	
+            to_exchange.println("HELLO RDFZFIFTEAN"); 
             String reply = from_exchange.readLine().trim();
             System.err.printf("The exchange replied: %s\n", reply);
         }
@@ -34,4 +32,5 @@ public class Bot
             e.printStackTrace(System.out);
         }
     }
+    
 }
