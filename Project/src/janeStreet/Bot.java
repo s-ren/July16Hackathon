bb31fe14006716aa4251b9f1e4ada048747dfed5
Ubuntu <ubuntu@bot-rdfzfifteen.sf.js-etc>
2016-07-16 18:27:02 +0000

    modified teamname

--- Project/src/janeStreet/Bot.java
+++ Project/src/janeStreet/Bot.java
@@ -12,7 +12,7 @@ public class Bot
     {
         try
         {
-            Socket skt = new Socket("test-exch-teamname", 20000);
+            Socket skt = new Socket("test-exch-rdfzfifteen", 20000);
             BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
             PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
 
